fc5e34e710e0252b5dabdd9aed03506dcdb3f3b3
Joel Wasserman <joel.wasserman8@gmail.com>
2018-12-01 15:56:11 -0800

    update

--- pages/contact.js
+++ pages/contact.js
@@ -2,11 +2,21 @@ import Nav from '../components/nav'
 
 const Contact = () => (
   <div>
-    <Nav />
+    <Nav title="Contact"/>
 
-    <div>
+    <div className="title">
       <h1>Contact Us</h1>
+      <div className="description">
+        Please please please help us improve! All suggestions welcome.<br/><br/>
+        joel.wasserman8@gmail.com
+      </div> 
     </div>
+    <style jsx>{`
+      .title,
+      .description {
+        text-align: center;
+      }
+    `}</style>
   </div>
 )
 
--- pages/index.js
+++ pages/index.js
@@ -7,54 +7,59 @@ const Home = () => (
     <Head title="Home" />
     <Nav />
 
-    <div className="hero">
-      <h1 className="title">Teacher Fund</h1>
+    <h1 className="title">Teacher Fund</h1>
+    <div className="body">
       <p className="description">
-        Supplemental funding for the public school education system via structured avenues
+        Supplemental funding for the public school education system via structured avenues. With a mission 
+        to support teachers in a way that encourages great teachers to stay, and potentially great 
+        teachers to choose teaching as a career path.
       </p>
-
       <div className="whitepaper">
         In School District 271 (future reference SD271) teachers have come to rely more and more on personal 
-        finances to fund their classroom. The current Idaho teacher salary is $42,000, an embarrassingly low 
-        amount considering the impact teachers have on students, the most optimistic promise for the future. 
-        TeacherFund hopes to supplement teacher federal funding by providing school supplies, for free, to 
-        teachers across SD271. Teachers need just select the products they need, put in a valid school address, 
-        and the products will be shipped to them. No questions asked. 
+        finances to fund their classroom. Initial TeacherFund work will be devoted towards providing supplemental teacher funding by providing school supplies, for free, to 
+        new public school teachers across SD271. 
         <br /><br />
-        TeacherFund is a 501c3 nonprofit corporation, so all donations are tax deductible. additionally, 
-        you will be directly helping sow the seeds of education for kids across SD271, and with the hope 
+        TeacherFund is a non-profit Washington based corporation. By donating, you will be 
+        directly improving the educational environment for kids across SD271, with the hope 
         of eventually providing supplemental funding across the country. 
         We recommend a monthly donation of ~$20, or whatever your economic status can sustain.
         Excess funding will be pooled together to support after school programs, overseen by our team at 
-        TeacherFund directly as to ensure accurate and efficient use of funds. Blog posts of progress will be posted. 
-
-        Lastly, I plea to the public to provide these resources. Many of us, who are now successful, were 
-        fortunate enough to grow up in a supporting home that supplemented our public school education. 
-        I hope we have the foresight to pay it forward for the next generation of kids, kids we hope to 
-        provide enough resources so they can solve the next generation of social and scientific challenges. 
+        TeacherFund directly as to ensure accurate and efficient use of funds.
+        <br /><br />
+        <h3>What we bring to the table.</h3>
+        <br />
+        Having an engineering based education, we'd like to incorporate technology as much as possible to
+        improve the donating / non profit experience. Additionally, we believe very heavily in transparency.
+        Along that theme, we will provide a "Finances" page in the coming months. This page will list all cash
+        in-flows and out-flows with anonymized data. If you're ever curious where our money is spent,
+        it shouldn't be hard to find out.
+        <br />
+        On top of that, we will be providing a "cashmap". If you'd like to login and become a recurring donation
+        member, you will be able to see directly where your dollars are spent on a rendered map. Zoom in and see
+        pictures and blog posts from the teachers that YOUR money, dollar for dollar, is affecting. 
       </div>
     </div>
 
     <style jsx>{`
-      .hero {
-        width: 100%;
+      .body {
         color: #333;
+        margin: 10%
       }
       .title {
         margin: 0;
         width: 100%;
-        padding-top: 80px;
+        text-align: center;
         line-height: 1.15;
         font-size: 48px;
       }
-      .title,
       .description {
-        text-align: center;
+        text-align: left;
+        font-weight: bold;
+        margin-bottom: 50px;
       }
       .whitepaper {
         flex-direction: row;
-        text-align: center;
-        margin: 60px;
+        text-align: left;
       }
     `}</style>
   </div>
