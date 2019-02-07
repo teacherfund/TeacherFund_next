3de67efbb5a2f8256e24302748a920c2f4aba3f7
Joel Wasserman <joel.wasserman8@gmail.com>
2019-02-07 15:13:44 -0800

    added stripe source to head

--- components/head.js
+++ components/head.js
@@ -34,6 +34,7 @@ const Head = props => (
       <meta property="og:image" content={props.ogImage || defaultOGImage} />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
+      <script src="https://js.stripe.com/v3/"></script>
     </NextHead>
   </div>
 )
