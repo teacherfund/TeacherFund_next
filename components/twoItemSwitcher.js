eda51264a0d0cb17202b292057bc88a8b3257162
Joel Wasserman <joel.wasserman8@gmail.com>
2019-02-13 09:01:39 -0800

    cleanup

--- components/twoItemSwitcher.js
+++ components/twoItemSwitcher.js
@@ -3,10 +3,10 @@ import '../static/styles/main.scss'
 
 const TwoItemSwitcher = (props) => (
   <div className="switcher">
-    <div className={`switcher__leftItem ${props.selectedToggle === 1 ? 'selected' : ''}`} onClick={props.switchOneClicked}>
+    <div className={`switcher__leftItem white ${props.selectedToggle === 1 ? 'selected' : ''}`} onClick={props.switchOneClicked}>
       <label>{props.switchOneText.toUpperCase()}</label>
     </div>
-    <div className={`switcher__rightItem ${props.selectedToggle === 2 ? 'selected' : ''}`} onClick={props.switchTwoClicked}>
+    <div className={`switcher__rightItem white ${props.selectedToggle === 2 ? 'selected' : ''}`} onClick={props.switchTwoClicked}>
       <label>{props.switchTwoText.toUpperCase()}</label>
     </div>
   </div>
