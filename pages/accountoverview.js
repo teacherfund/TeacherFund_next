e668b08f481ab56b51782138466007d732b24a39
Joel Wasserman <joel.wasserman8@gmail.com>
2019-02-14 14:29:05 -0800

    added in account overview page skeleton

---
+++ pages/accountoverview.js
@@ -0,0 +1,21 @@
+import Nav from '../components/nav'
+import Footer from '../components/footer'
+import Head from '../components/head'
+import '../static/styles/main.scss'
+
+const Accountoverview = () => (
+  <div className="main-container">
+    <Nav />
+    <Head title="Account" />
+
+    <div className="body">
+      <h1>Account Overview</h1>
+      <div className="description">
+
+      </div> 
+    </div>
+    <Footer />
+  </div>
+)
+
+export default Accountoverview
