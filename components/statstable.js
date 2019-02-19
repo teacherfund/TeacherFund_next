b0e420c37354e8da9ecbc5d17757fc64b3f2c5da
Joel Wasserman <joel.wasserman8@gmail.com>
2019-02-18 22:29:57 -0800

    added in stats table for our work page

--- components/statstable.js
+++ components/statstable.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import '../static/styles/main.scss'
 
+// pull stats from API
 const stats = [
-  {},
-  {},
-  {},
-  {}
+  {
+    stat: '4000',
+    subtext: 'Dollars towards teacher supplies'
+  },
+  {
+    stat: '4000',
+    subtext: 'Dollars towards life readiment courses'
+  },
+  {
+    stat: '4',
+    subtext: 'Local partner schools'
+  },
+  {
+    stat: '1269',
+    subtext: 'Impacted children'
+  }
 ]
 
 const StatsTable = (props) => (
   <div className='statstable'>
-    {stats.map(({ key, title, subtext }) => (
-      <div className='stat'>
-        <p className='p40 title' />
-        <p className='p40 description' />
+    {stats.map(({ key, stat, subtext }) => (
+      <div className='statstable--stat' key={key}>
+        <p className='p40 statstable--stat--number'>{stat}</p>
+        <p className='p40 statstable-stat--subtext'>{subtext}</p>
       </div>
     ))}
   </div>
