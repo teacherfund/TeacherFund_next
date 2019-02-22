45693d3aca4b964830f4b87516e98a66dee140c9
Joel Wasserman <joel.wasserman8@gmail.com>
2019-02-22 08:39:33 -0800

    update

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -3,12 +3,18 @@ import Footer from '../components/footer'
 import Head from '../components/head'
 import '../static/styles/main.scss'
 
+const employees = [
+  { picture: '/', name: 'Joel Wasserman', description: '' },
+  { picture: '/', name: 'Christine Woeller', description: '' },
+  { picture: '/', name: 'Peter Squicciarini', description: '' }
+]
+
 const About = () => (
   <div className='main-container'>
     <Nav navColor='black' />
     <Head title='About' />
 
-    <div className='body'>
+    <div className='body aboutusBody'>
       <div className='mission'>
         <div className='mission--header'>
           Our Mission
@@ -20,10 +26,54 @@ const About = () => (
         </div>
       </div>
       <div className='factcards'>
-
+        <div className="factcard--container">
+          <div className="factcard--fact--image"></div>
+          <div className="factcard--fact--title">
+            <h3>We prove every project</h3>
+          </div>
+          <div className="factcard--fact--text">
+            <p className="p40">
+              We track every dollar spent and received, and show
+              the projects you fund with photos and GPS.
+            </p>
+          </div>
+        </div>
+        <div className="factcard--container">
+          <div className="factcard--fact--image"></div>
+          <div className="factcard--fact--title">
+            <h3>We're an open book</h3>
+          </div>
+          <div className="factcard--fact--text">
+            <p className="p40">
+              View our cash inflows and outflows down to the
+              penny, at any time. We welcome audits! No more donating into a black box.
+            </p>
+          </div>
+        </div>
+        <div className="factcard--container">
+          <div className="factcard--fact--image"></div>
+          <div className="factcard--fact--title">
+            <h3>100% goes into the field</h3>
+          </div>
+          <div className="factcard--fact--text">
+            <p className="p40">
+              Private donors cover our operating costs so 100%
+              of your money goes towards public school education.
+            </p>
+          </div>
+        </div>
       </div>
       <div className='us'>
-
+        <div className="title"></div>
+        <div className="employee--container">
+          {employees.map(({ image, name, description }) => (
+            <div key={name}>
+              <div className="employee--image"></div>
+              <div className="employee--name"></div>
+              <div className="employee--description"></div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
     <Footer />
