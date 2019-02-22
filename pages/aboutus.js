978455d67cdbdc0f975c5ad0eaeacb78d165eb67
Joel Wasserman <joel.wasserman8@gmail.com>
2019-02-22 10:30:14 -0800

    update

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -20,43 +20,43 @@ const About = () => (
           Our Mission
         </div>
         <div className='mission--body'>
-          The Teacher Fund is a non-profit organization 
-          bringing school supplies and supplemental 
+          The Teacher Fund is a non-profit organization
+          bringing school supplies and supplemental
           funding to public school teachers.
         </div>
       </div>
       <div className='factcards'>
-        <div className="factcard--container">
-          <div className="factcard--fact--image"></div>
-          <div className="factcard--fact--title">
+        <div className='factcard--container'>
+          <div className='factcard--fact--image' />
+          <div className='factcard--fact--title'>
             <h3>We prove every project</h3>
           </div>
-          <div className="factcard--fact--text">
-            <p className="p40">
+          <div className='factcard--fact--text'>
+            <p className='p40'>
               We track every dollar spent and received, and show
               the projects you fund with photos and GPS.
             </p>
           </div>
         </div>
-        <div className="factcard--container">
-          <div className="factcard--fact--image"></div>
-          <div className="factcard--fact--title">
+        <div className='factcard--container'>
+          <div className='factcard--fact--image' />
+          <div className='factcard--fact--title'>
             <h3>We're an open book</h3>
           </div>
-          <div className="factcard--fact--text">
-            <p className="p40">
+          <div className='factcard--fact--text'>
+            <p className='p40'>
               View our cash inflows and outflows down to the
               penny, at any time. We welcome audits! No more donating into a black box.
             </p>
           </div>
         </div>
-        <div className="factcard--container">
-          <div className="factcard--fact--image"></div>
-          <div className="factcard--fact--title">
+        <div className='factcard--container'>
+          <div className='factcard--fact--image' />
+          <div className='factcard--fact--title'>
             <h3>100% goes into the field</h3>
           </div>
-          <div className="factcard--fact--text">
-            <p className="p40">
+          <div className='factcard--fact--text'>
+            <p className='p40'>
               Private donors cover our operating costs so 100%
               of your money goes towards public school education.
             </p>
@@ -64,13 +64,13 @@ const About = () => (
         </div>
       </div>
       <div className='us'>
-        <div className="title"></div>
-        <div className="employee--container">
+        <div className='title' />
+        <div className='employee--container'>
           {employees.map(({ image, name, description }) => (
             <div key={name}>
-              <div className="employee--image"></div>
-              <div className="employee--name"></div>
-              <div className="employee--description"></div>
+              <div className='employee--image' />
+              <div className='employee--name' />
+              <div className='employee--description' />
             </div>
           ))}
         </div>
