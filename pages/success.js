f3e943460eff0740479e44d762c90b99c791a6c7
Joel Wasserman <joel.wasserman8@gmail.com>
2019-03-05 07:50:41 -0800

    added success page styling

--- pages/success.js
+++ pages/success.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 import Head from '../components/head'
 import Nav from '../components/nav'
 
@@ -12,7 +13,16 @@ class Success extends React.Component {
         <Head title='Success' />
         <Nav navColor='black' />
         <div className='body'>
-          <p>Success</p>
+          <img src='/static/images/einstein.jpg' className='backgroundImage full-height' />
+          <div className='success'>
+            <p className='pb-1'>Thank you! Your donation helps teachers and students across the country.</p>
+            <p className='pb-1'>If you have an account, we'll be in touch when your funds are spent so you can see exactly what your money is being spent on</p>
+            <div className='button--subsection white'>
+              <Link href='/'>
+                <label className='ttu'>Home</label>
+              </Link>
+            </div>
+          </div>
         </div>
 
         <Footer />
