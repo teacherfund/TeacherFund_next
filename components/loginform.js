ef034cfb53fa1ad5163dce47df0577d8ca71fa84
Joel Wasserman <joel.wasserman8@gmail.com>
2019-03-13 08:38:59 -0700

    added in api route and handling in index.js of magic link query string params

--- components/loginform.js
+++ components/loginform.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react'
 import TwoItemSwitcher from './twoItemSwitcher'
+import * as Api from '../api/api'
 import '../static/styles/main.scss'
 
 class LoginForm extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      name: '',
-      user: '',
-      pass: '',
+      firstName: '',
+      lastName: '',
+      email: '',
       register: false,
       loginType: props.type
     }
   }
 
+  setLocalState = (state) => {
+    if (!state.error) state.error = ''
+    this.setState(state)
+  }
+
+  updateFirstname = (e) => {
+    this.setLocalState({ firstName: e.target.value })
+  }
+
+  updateLastname = (e) => {
+    this.setLocalState({ lastName: e.target.value })
+  }
+
+  updateEmail = (e) => {
+    this.setLocalState({ email: e.target.value })
+  }
+
   updateLoginType = (newVal) => {
-    this.setState({ loginType: newVal })
+    this.setLocalState({ loginType: newVal })
   }
 
   updateFormType = () => {
-    this.setState({ register: !this.state.register })
+    this.setLocalState({ register: !this.state.register })
   }
 
   handleSubmit = () => {
@@ -31,12 +49,32 @@ class LoginForm extends Component {
     }
   }
 
-  login = () => {
-
+  login = async () => {
+    try {
+      const responseStream = await Api.login({
+        email: this.state.email,
+        role: this.state.loginType
+      })
+      const response = await responseStream.json()
+      console.log(response)
+    } catch (e) {
+      console.error(e)
+    }
   }
 
-  signup = () => {
-
+  signup = async () => {
+    try {
+      const responseStream = await Api.register({
+        email: this.state.email,
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        role: this.state.loginType
+      })
+      const response = await responseStream.json()
+      console.log(response)
+    } catch (e) {
+      console.error(e)
+    }
   }
 
   render () {
@@ -59,39 +97,44 @@ class LoginForm extends Component {
         <form onSubmit={this.handleSubmit}>
           <div className='panel'>
             {this.state.register &&
-            <div className='input-wrapper'>
-              <input
-                required='required'
-                className='name'
-                aria-required='true'
-                placeholder='Full Name'
-                name='user[name]'
-                id='user_name'
-                autoComplete='off'
-              />
+            <div>
+              <div className='input-wrapper'>
+                <input
+                  required='required'
+                  className='name'
+                  value={this.state.firstName}
+                  aria-required='true'
+                  placeholder='First name'
+                  name='first name'
+                  id='first_name'
+                  autoComplete='off'
+                />
+              </div>
+              <div className='input-wrapper'>
+                <input
+                  required='required'
+                  className='name'
+                  value={this.state.lastName}
+                  aria-required='true'
+                  placeholder='Last name'
+                  name='last name'
+                  id='last_name'
+                  autoComplete='off'
+                />
+              </div>
             </div>}
             <div className='input-wrapper'>
               <input
                 required='required'
                 className='email'
+                value={this.state.email}
                 aria-required='true'
                 placeholder='Email'
                 type='email'
-                name='user[email]'
+                name='email'
                 id='user_email'
                 autoComplete='off'
               />
-            </div>
-            <div className='input-wrapper'>
-              <input
-                className='password'
-                autoComplete='off'
-                placeholder='Password'
-                type='password'
-                name='user[password]'
-                id='user_password'
-              />
-              <a className='toggle' data-hidden='true'>Show</a>
             </div>
             <input
               type='submit'
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Head from '../components/head'
 import Nav from '../components/nav'
 import SubsectionBasic from '../components/subsectionBasic'
+import * as Api from '../api/api'
 import SubsectionImportant from '../components/subsectionImportant'
 import EmailCapture from '../components/emailCapture'
 
@@ -11,36 +12,57 @@ import '../static/styles/main.scss'
 
 const emailTitle = 'Help us change the world.'
 
-const Home = () => (
-  <div className='main-container'>
-    <Head title='Home' />
-    <Nav navColor='white' />
-    <div className='body'>
-      <SubsectionBasic
-        titleText='100% of your money brings school supplies to teachers and students in need.'
-        descriptionText='You can help aleviate economic pressure on public school teachers across
-          the country. Every penny will help bring school supplies to schools in need.'
-        buttonText='Give monthly'
-        image='/static/images/apple-blur-book-stack.jpg'
-        destination='donate'
-      />
-      <div className='emailCapture'>
-        <h2 className='emailCapture--title'>
-          {emailTitle}
-        </h2>
-        <EmailCapture />
+class Home extends React.Component {
+  getInitialProps ({ query }) {
+    // If query string has auth verify and email param then send verify request to api
+    if (query.auth && query.email) {
+      this.verifyEmail(query)
+    }
+  }
+
+  async verifyEmail (query) {
+    try {
+      const responseStream = await Api.verify({ auth: query.auth, email: query.email })
+      const res = responseStream.json()
+      console.log(res)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  render () {
+    return (
+      <div className='main-container'>
+        <Head title='Home' />
+        <Nav navColor='white' />
+        <div className='body'>
+          <SubsectionBasic
+            titleText='100% of your money brings school supplies to teachers and students in need.'
+            descriptionText='You can help aleviate economic pressure on public school teachers across
+              the country. Every penny will help bring school supplies to schools in need.'
+            buttonText='Give monthly'
+            image='/static/images/apple-blur-book-stack.jpg'
+            destination='donate'
+          />
+          <div className='emailCapture'>
+            <h2 className='emailCapture--title'>
+              {emailTitle}
+            </h2>
+            <EmailCapture />
+          </div>
+
+          <SubsectionImportant
+            titleText='Ninety-four percent of public school teachers say they spent their own money on notebooks, pens and other supplies in the 2014-15 school year without reimbursement.'
+            descriptionText="We're on a mission to change that. Here's how."
+            image='/static/images/blur-child-classroom.jpg'>
+            <CustomSwiper />
+          </SubsectionImportant>
+        </div>
+
+        <Footer />
       </div>
-
-      <SubsectionImportant
-        titleText='Ninety-four percent of public school teachers say they spent their own money on notebooks, pens and other supplies in the 2014-15 school year without reimbursement.'
-        descriptionText="We're on a mission to change that. Here's how."
-        image='/static/images/blur-child-classroom.jpg'>
-        <CustomSwiper />
-      </SubsectionImportant>
-    </div>
-
-    <Footer />
-  </div>
-)
+    )
+  }
+}
 
 export default Home
