95f88f58afeca4eab8245391ab4371a0eeaa2fb6
Abdirahman Guledm <abdishire15@gmail.com>
2019-03-29 15:51:38 -0400

    created new component at component/hourGlass.js

---
+++ components/hourGlass.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../static/styles/main.scss'
+
+const hourGlass = () => (
+    <div className="hourglass">
+          <div className="top"></div>
+          <div className="bottom"></div>
+    </div>
+)
+
+export default hourGlass
