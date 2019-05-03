1d6b08951b11e14e7e2b4f93b05163cabec7e5bb
Joel Wasserman <joel.wasserman8@gmail.com>
2019-05-03 15:00:41 +0200

    cleanup

--- pages/ourwork.js
+++ pages/ourwork.js
@@ -1,4 +1,4 @@
-// import React from 'react'
+import React from 'react'
 // import Nav from '../components/nav'
 // import Footer from '../components/footer'
 // import Head from '../components/head'
@@ -13,39 +13,23 @@
 // receive the recognition they deserve. We're working to change that. Teacher Fund hopes to encourage students to pursue their
 // passions while providing resources and support for STEM related fields.`
 
-// class Ourwork extends React.Component {
-//   render () {
-//     return (
-//       <div className='main-container'>
-//         <Nav navColor='white' />
-//         <Head title='Our Work' />
+/* <div className='mapgraphic--container'>
+  <h2 className='white center pb-3'>Where we operate</h2>
+  <MapGraphic
+    isMarkerShown
+    googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${APIKEY}&callback=initMap`}
+    loadingElement={<div style={{ height: `100%` }} />}
+    containerElement={<div style={{ height: `400px` }} />}
+    mapElement={<div style={{ height: `100%` }} />}
+  />
+</div> */
 
-//         <div className='body'>
-//           <SubsectionImportant
-//             backgroundImageClasses='fade-wht full-height'
-//             titleText='How we work'
-//             image='/static/images/beakers.jpg'
-//             descriptionText={subsectionText}
-//           >
-//             <StatsTable globalAmountDonated={this.props.context.globalAmountDonated} globalAmountSpent={this.props.context.globalAmountSpent} />
-//             {/* <TFTable data={this.state.data} /> */}
-//           </SubsectionImportant>
+class Ourwork extends React.Component {
+  render () {
+    return (
+      <div className='main-container' />
+    )
+  }
+}
 
-//           <div className='mapgraphic--container'>
-//             <h2 className='white center pb-3'>Where we operate</h2>
-//             <MapGraphic
-//               isMarkerShown
-//               googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${APIKEY}&callback=initMap`}
-//               loadingElement={<div style={{ height: `100%` }} />}
-//               containerElement={<div style={{ height: `400px` }} />}
-//               mapElement={<div style={{ height: `100%` }} />}
-//             />
-//           </div>
-//         </div>
-//         <Footer />
-//       </div>
-//     )
-//   }
-// }
-
-// export default Ourwork
+export default Ourwork
