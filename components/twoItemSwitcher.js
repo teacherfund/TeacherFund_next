e62eb22b5f29ac4cb7cfdb4e1d3afc1ecb5098a4
Joel Wasserman <joel.wasserman8@gmail.com>
2019-05-05 07:57:46 +0200

    added dynamic pointer styles for two item picker

--- components/twoItemSwitcher.js
+++ components/twoItemSwitcher.js
@@ -3,11 +3,11 @@ import '../static/styles/main.scss'
 
 const TwoItemSwitcher = (props) => (
   <div className='flex flex-row tf-lato justify-between ma1'>
-    <div className={`w-100 h2 tc mt1 br--left br2 ${props.color} ${props.selectedToggle === 1 ? 'tf-dark-gray bg-white' : 'bg-tf-yellow white'}`} onClick={props.switchOneClicked}>
-      <label className='ttu v-mid'>{props.switchOneText}</label>
+    <div className={`w-100 h2 tc mt1 br--left br2 ${props.color} ${props.selectedToggle === 1 ? 'bg-tf-yellow white' : 'tf-dark-gray bg-white pointer'}`} onClick={props.switchOneClicked}>
+      <label className={`ttu v-mid ${props.selectedToggle === 1 ? '' : 'pointer'}`}>{props.switchOneText}</label>
     </div>
-    <div className={`w-100 h2 tc mt1 br--right br2 ${props.color} ${props.selectedToggle === 2 ? 'tf-dark-gray bg-white' : 'bg-tf-yellow white'}`} onClick={props.switchTwoClicked}>
-      <label className='ttu v-mid'>{props.switchTwoText}</label>
+    <div className={`w-100 h2 tc mt1 br--right br2 ${props.color} ${props.selectedToggle === 2 ? 'bg-tf-yellow white' : 'tf-dark-gray bg-white pointer'}`} onClick={props.switchTwoClicked}>
+      <label className={`ttu v-mid ${props.selectedToggle === 2 ? '' : 'pointer'}`}>{props.switchTwoText}</label>
     </div>
   </div>
 )
