59c11b708c2ad063a5c6a695e01624d386172559
Joel Wasserman <joel.wasserman8@gmail.com>
2019-06-22 22:03:22 -0700

    linted

--- components/nav.js
+++ components/nav.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Hamburger from './icons/hamburger'
+import X from './icons/x'
 import Link from 'next/link'
 
 class Nav extends React.Component {
@@ -6,64 +8,67 @@ class Nav extends React.Component {
     super(props)
 
     this.links = [
-      // { href: '/', label: 'Home' },
-      // { href: '/signinregister?type=donor', label: 'Login', key: 'nav-link-login' }
-      // { href: '/whyteachers', label: 'Why Teachers?' },
-      // { href: '/ourwork', label: 'Our Work' },
-      // { href: '/aboutus', label: 'About us' }
+      { href: '/mission', label: 'Our Mission' }
     ]
 
     this.buttons = [
       { href: '/donate', label: 'Donate', key: 'nav-link-donate' }
-      // { href: '/signinregister?type=teacher', label: 'I\'m a teacher' }
     ]
-    this.loggedIn = false
+    this.state = {
+      drawerOpen: false
+    }
   }
 
-  componentDidMount () {
-    if (this.props.loggedIn && !this.loggedIn) this.switchLoginToAccount()
-  }
-
-  componentDidUpdate () {
-    if (this.props.loggedIn && !this.loggedIn) this.switchLoginToAccount()
-  }
-
-  switchLoginToAccount = () => {
-    // If user is logged in, splice out login button and add in an "account" button
-    this.buttons.splice(1, 1,
-      { key: 'nav-link-/account-account', href: '/account', label: 'Account' }
-    )
-    this.loggedIn = true
+  toggleDrawerOpen = () => {
+    console.log(this.state.drawerOpen)
+    this.setState({ drawerOpen: !this.state.drawerOpen })
   }
 
   render () {
     return (
-      <div className='f6 tf-lato bg-white pv4 flex fl w-100 pl5-ns pr5-ns pl2 pr2 pt4 z-1'>
-        <div className='b--tf-yellow flex justify-between flex-row w-100'>
-          <div className='pointer tc'>
-            <Link href='/'>
-              <img src='/static/images/Logo_with_text.png' className='w4' />
-            </Link>
-          </div>
-          <div className='fr flex-column flex-auto-ns mv-auto'>
-            {this.links.map(({ key, href, label }) => (
-              <div key={key} className='db center w-auto fr ph2'>
-                <Link href={href}>
-                  <a className='tf-dark-gray no-underline black pa3'>{label}</a>
-                </Link>
+      <div>
+        <div className='f6 tf-lato bg-white pv4 flex fl w-100 pl5-ns pr5-ns pl2 pr2 pt4 z-1'>
+          <div className='b--tf-yellow flex justify-between flex-row w-100'>
+            <div className='pointer tc'>
+              <Link href='/'>
+                <img src='/static/images/Logo_with_text.png' className='w4' />
+              </Link>
+            </div>
+            <div className='fr dn db-l flex-column flex-auto-ns mv-auto'>
+              {this.links.map(({ key, href, label }) => (
+                <div key={key} className='db center w-auto fr ph2'>
+                  <Link href={href}>
+                    <a className='tf-dark-gray no-underline black pa3'>{label}</a>
+                  </Link>
+                </div>
+              ))}
+            </div>
+            <div className='fr dn db-l tf-yellow mv-auto'>
+              {this.buttons.map(({ key, href, label }) => (
+                <div key={key} className='db center w-auto fr ph2'>
+                  <Link href={href} key={key}>
+                    <a className='b--tf-yellow tf-yellow no-underline black pv3 ph4 ba bw2 br3'>{label}</a>
+                  </Link>
+                </div>
+              ))}
+            </div>
+            <div className='fr dn-l db'>
+              <div className='pa3' onClick={this.toggleDrawerOpen}>
+                <Hamburger />
               </div>
-            ))}
-          </div>
-          <div className='fr tf-yellow mv-auto'>
-            {this.buttons.map(({ key, href, label }) => (
-              <div key={key} className='db center w-auto fr ph2'>
-                <Link href={href} key={key}>
-                  <a className='b--tf-yellow tf-yellow no-underline black pv3 ph4 ba bw2 br3'>{label}</a>
-                </Link>
-              </div>
-            ))}
+            </div>
           </div>
         </div>
+        {this.state.drawerOpen && <div className='w-100 h-100 bg-tf-dark-gray o-90 absolute white tf-lato tc'>
+          <div className='fr pa4' onClick={this.toggleDrawerOpen}>
+            <X />
+          </div>
+          <div className='flex-column flex justify-around h4 mt6'>
+            <p><a href='/' className='white no-underline'>Home</a></p>
+            <p><a href='/donate' className='white no-underline'>Donate</a></p>
+            <p><a href='/mission' className='white no-underline'>Our Mission</a></p>
+          </div>
+        </div>}
       </div>
     )
   }
---
+++ pages/mission.js
@@ -0,0 +1,40 @@
+import Nav from '../components/nav'
+import { Component } from 'react'
+import Head from '../components/head'
+import '../static/styles/main.scss'
+
+class Mission extends Component {
+  render () {
+    return (
+      <div>
+        <Head title='Mission' />
+        <Nav />
+        <div className='w-100 h-100 flex-column flex'>
+          <div className='w-100 h-100 bg-tf-gray o-10 absolute z-minus-1' />
+          <div className='tf-dark-gray tc w-100 ts-title tf-oswald fl pa4'>
+            Our Mission
+          </div>
+          <div className='flex-row tf-lato-lite pb6-ns pl6-ns pr6-ns pa3'>
+            <p>Did you know that in 2015, 94% of public school teachers spent their own money on school supplies? We live in a country where teachers aren't given the resources to properly educate our children. How is this possible?</p>
+
+            <p>About six months ago, a few friends and I started TeacherFund: a WA non-profit, with the hope of positively impacting public school education, primarily through financially aiding teachers.</p>
+
+            <p>As I mentioned earlier, 94% of public school teachers spent <a href='https://www.nytimes.com/2018/05/16/us/teachers-school-supplies.html'>their own money on school supplies in 2015</a>. Their own money! These are public school teachers making on average 35k a year, and they're spending their own money on school supplies trying to make their classroom a better learning environment. THIS IS INSANE.</p>
+
+            <p>Our impact begins in Idaho. We will start by supplying classroom supplies to all new teachers in school district 271. This is a baby step, albeit an important one, but a baby step nonetheless, and we acknowledge that.</p>
+
+            <p>This baby step will help us gain insights into the public school education system that we wouldn't otherwise have. Insights that will lead to slightly bigger steps, and on and on.</p>
+
+            <p>If any of this resonates with you, let’s take action and make a difference. If you remember the teacher that believed in you, the one that wrote you a letter of recommendation, or the one that helped you to dream big, consider donating.</p>
+
+            <p>I talk a lot about being a dreamer. What I don't talk a lot about is what enabled me to have big dreams in the first place. It started with school; I was the kid in 4th grade dreaming of walking on Mars. And although I failed the history test later that week because I was daydreaming, it all surmounted to one thing; I was empowered to dream. The reason I had that power was because I had teachers that showed me what was possible in this world. They encouraged me to never shy away from pushing the envelope. Now we're dreaming again, and we have this crazy notion that we can bootstrap a non-profit that could positively impact the lives of hundreds of thousands of kids.</p>
+
+            <p>Every dream sounds crazy when you first say it out loud. We just did that. Now we're going to make it a reality.</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Mission
