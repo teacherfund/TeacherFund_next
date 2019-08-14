20e0e26209391429e620e825790ef1e97aa56a3c
Joel Wasserman <joel.wasserman8@gmail.com>
2019-08-14 11:42:48 -0700

    Update donate.js

--- pages/donate.js
+++ pages/donate.js
@@ -46,8 +46,8 @@ class Donate extends Component {
                 <input type='hidden' name='hosted_button_id' value='5BZAW3NKL9L9W' />
                 <input className='paypal-button' type='image' src='https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif' border='0' name='submit' title='PayPal - The safer, easier way to pay online!' alt='Donate with PayPal button' />
                 <img alt='' border='0' src='https://www.paypal.com/en_US/i/scr/pixel.gif' width='1' height='1' />
-              </form>}
-            </div>
+              </form>
+            </div>}
           </div>
           }
         </div>
