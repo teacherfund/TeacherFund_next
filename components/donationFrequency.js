7b4eda58d92ff17d5651f3896c6b5a0b24295ede
meowwwls <meowwwls@users.noreply.github.com>
2019-10-07 00:02:04 -0500

    Make donation frequency options more accessible

--- components/donationFrequency.js
+++ components/donationFrequency.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const DonationFrequency = ( { updateFrequency, frequency }) => (
-  <div className='flex flex-row tf-lato justify-between ma1' role="radiogroup" aria-labelledby="donate-freq">
+const DonationFrequency = ({ updateFrequency, frequency }) => (
+  <div className='flex flex-row tf-lato justify-between ma1' role='radiogroup' aria-labelledby='donate-freq'>
     <span id='donate-freq' className='visually-hidden'>Donation Frequency</span>
     <label className={`w-100 h2 tc mt1 bn ba b--black ttu pointer flex flex-column justify-center ${frequency === 'once' ? 'bg-tf-yellow white' : 'tf-dark-gray bg-white pointer'}`}>
       <input className='visually-hidden' type='radio' name='frequency' value='once' onInput={updateFrequency} />
@@ -15,4 +15,3 @@ const DonationFrequency = ( { updateFrequency, frequency }) => (
 )
 
 export default DonationFrequency
-
