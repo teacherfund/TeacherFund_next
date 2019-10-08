e112ec656d841b25cccadd8c10ff694b3bfbd0a0
korlando7 <korlando7@gmail.com>
2019-10-08 14:53:42 -0400

    small screen styling

--- components/nav.js
+++ components/nav.js
@@ -26,7 +26,7 @@ class Nav extends React.Component {
   render () {
     return (
       <nav>
-        <div className='f6 f5-m tf-lato bg-white pv4 flex fl w-100 pl5-ns pr5-ns pl2 pr2 pt4 z-1'>
+        <div className='f6 f5-m tf-lato bg-white pv4 flex fl w-100 pl5-ns pr5-ns pl3 pr3 z-1'>
           <div className='b--tf-yellow flex justify-between flex-row w-100'>
             <div className='pointer tc'>
               <Link href='/'>
--- components/team.js
+++ components/team.js
@@ -4,18 +4,21 @@ import TeamMember from './teamMember'
 const teamMembers = [
   {
     name: 'Joel Wasserman',
+    title: 'Outreach Director',
     src: 'joel.jpg',
     bio: `A software engineer at Google, Joel formerly worked
           at two education startups and is passionate about public education and technology.`
   },
   {
     name: 'Christine Woeller',
+    title: 'Development Director',
     src: 'christine.jpeg',
     bio: `With a degree in secondary education, Christine seeks to improve public schools
           and ensure teachers have the best resources available.`
   },
   {
     name: 'Peter Squicciarini',
+    title: 'Program Director',
     src: 'pete.jpeg',
     bio: ` A software developer at Amazon and loving father of two, Peter strives to
           help all children and teachers succeed in an educational environment.`
@@ -24,15 +27,19 @@ const teamMembers = [
 
 const Team = (props) =>
   (
-    <section className='h-section mb5 mb0-ns pb5-ns bg-card'>
-      <div className='flex flex-column z-100'>
-        <div className='bg-tf-gray o-10 dn db-ns dn-m h-section w-100 absolute' />
-        <h2 className='f1-l f2-m mv3-m tf-oswald center'>
+    <section className='h-section mb0-ns pb5-ns bg-card'>
+      <div className='flex flex-column z-100 pt4-m'>
+        <h2 className='f1-l f2 mv3-m tf-oswald center'>
         Meet the Team
         </h2>
-        <ul className='list flex flex-row flex-wrap justify-center w-auto mt3 mb0 pv0 ph0 center'>
+        <ul className='list flex flex-row flex-wrap justify-center w-auto mb0 pv0 ph0 center bg'>
           {teamMembers.map(member =>
-            <TeamMember name={member.name} src={member.src} bio={member.bio} />)
+            <TeamMember
+              name={member.name}
+              title={member.title}
+              src={member.src}
+              bio={member.bio}
+            />)
           }
         </ul>
       </div>
