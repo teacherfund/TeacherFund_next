ab555f86b8f7837266e28145dfb6efd1db7acdc3
dreamerchandra <17010408+dreamerchandra@users.noreply.github.com>
2019-10-11 04:05:55 +0530

    added comments

--- components/Blog/blogPage.js
+++ components/Blog/blogPage.js
@@ -6,20 +6,20 @@ import React from 'react'
 function useToRenderBlog () {
   const ALL_SCHOOL = 'All'
   const blogs = useBlogContent()
-  const [blogCountries, updateSchool] = React.useState([ALL_SCHOOL])
-  const [selectedSchool, updateSelectedSchool] = React.useState(ALL_SCHOOL)
+  const [schools, updateSchool] = React.useState([ALL_SCHOOL]) // dynamically get unique schools from the blog response
+  const [selectedSchool, updateSelectedSchool] = React.useState(ALL_SCHOOL) // if user selects a particular school from the sideNav blogs related to selected schools should be shown
   React.useEffect(() => {
     const uniqueSchool = [ALL_SCHOOL, ...new Set(blogs.map(blog => blog.school))]
     updateSchool(() => uniqueSchool)
   }, [blogs])
-  return [blogs, blogCountries, selectedSchool, updateSelectedSchool]
+  return [blogs, schools, selectedSchool, updateSelectedSchool]
 }
 
 function BlogPage () {
-  const [blogs, blogSchools, selectedSchool, updateSelectedSchool] = useToRenderBlog()
+  const [blogs, schools, selectedSchool, updateSelectedSchool] = useToRenderBlog()
   return (
-    <div className='blog-page'>
-      <SideNav blogSchools={blogSchools} selectedSchool={selectedSchool} onSchoolSelected={updateSelectedSchool} />
+    <div className='flex'>
+      <SideNav schools={schools} selectedSchool={selectedSchool} onSchoolSelected={updateSelectedSchool} />
       <BlogList blogs={blogs} selectedSchool={selectedSchool} />
     </div>
   )
