64d1f839583e1520f6e4cc83b5a059258ae84c65
dreamerchandra <17010408+dreamerchandra@users.noreply.github.com>
2019-10-11 12:56:06 +0530

    refactor for styles

--- components/Blog/SideNav.js
+++ components/Blog/SideNav.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 function SideNav ({ schools, selectedSchool, onSchoolSelected }) {
   return (
-    <div className='side-nav-blog flex flex-column w-40 self-center pa2'>
+    <div className='flex flex-column w-20 self-center pa2'>
       {
         schools.map((school) => {
           const isCurrentSchoolSelected = school === selectedSchool
-          const className = `${isCurrentSchoolSelected ? 'selected-school' : ''} pa2 tc ttc mb1 bg-card`
+          const className = `${isCurrentSchoolSelected ? 'blog-side-nav-school-selected' : 'blog-side-nav-school-non-selected'} pa2 tc ttc mb1 bg-card`
           return (
-            <a className={className} key={school} data-school={school} onClick={(event) => onSchoolSelected(event.target.getAttribute('data-school'))}>
+            <a className={className} key={school} data-school={school} onClick={(event) => onSchoolSelected(school)}>
               {school}
             </a>
           )
