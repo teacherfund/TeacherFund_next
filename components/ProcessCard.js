d48e16a4a56d004b186d4f28f29fee6a6a44de22
Joel Wasserman <joel.wasserman8@gmail.com>
2020-10-09 22:11:43 -0700

    small change removing tachyons styles

--- components/ProcessCard.js
+++ components/ProcessCard.js
@@ -1,9 +1,10 @@
 import 'react'
+import { Box } from '@chakra-ui/core'
 
 export default function ProcessCard ({ title, children, icon }) {
   return (
     <div className='ma3 ma3-m w-25-m w-25-l'>
-      <div className='w-100 pa3 pt3-m bg-card h5-25'>
+      <Box className='pa3 pt3-m bg-card h5-25' width='100%'>
         <div className='pa2 tc'>
           <div className='center br-100 pa4 pa2-m ba b--black-05 mb2 ma3-m h4 h3-m w4 w3-m bg-white'>
             {icon}
@@ -11,7 +12,7 @@ export default function ProcessCard ({ title, children, icon }) {
           <div className='tf-oswald f3-l f4-m pv2 tc'>{title}</div>
           <div className='tf-lato-lite f3-l pt2 tc f5-m'>{children}</div>
         </div>
-      </div>
+      </Box>
     </div>
   )
 }
