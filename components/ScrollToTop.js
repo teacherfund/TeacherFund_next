dfc4f762d101cfdde75e225251db1d23ab772da8
poem <normamoras@gmail.com>
2020-10-18 06:11:20 -0400

    up arrow icon

---
+++ components/ScrollToTop.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../public/styles/main.scss'
+import { Flex, Icon } from '@chakra-ui/core'
+// import styles from './ScrollToTop.module.scss'
+
+const ScrollToTop = () => {
+  return (
+    <Flex>
+      <Icon name='arrow-up' size='100px' color='#f6b333' />
+
+    </Flex>
+  )
+}
+
+export default ScrollToTop
