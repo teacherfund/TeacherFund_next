92bb2ad867f6a153420f0fef0515fdbde8771943
Minh <s3722599@student.rmit.edu.au>
2020-10-27 16:48:48 +1100

    ProcessCard height to 100%

--- components/ProcessCard.js
+++ components/ProcessCard.js
@@ -3,7 +3,7 @@ import 'react'
 export default function ProcessCard ({ title, children, icon }) {
   return (
     <div className='ma3 ma3-m w-25-m w-25-l'>
-      <div className='w-100 pa3 pt3-m bg-card h5-25'>
+      <div className='w-100 pa3 pt3-m bg-card min-h-100'>
         <div className='pa2 tc'>
           <div className='center br-100 pa4 pa2-m ba b--black-05 mb2 ma3-m h4 h3-m w4 w3-m bg-white'>
             {icon}
