c1afe62ee6892683acc89ee477cf670b05332a8f
Oladipupoak <oladipupo.akinyan@kibo.school>
2026-02-24 21:16:00 +0100

    feat: implement pagination for customer charges retrieval

--- pages/api/stripe/donations/user.js
+++ pages/api/stripe/donations/user.js
@@ -21,11 +21,8 @@ export default async (req, res) => {
     if (!customer) {
       return res.json(customerTransactions)
     }
+
     const { id, metadata } = customer
-    const opts = {
-      customer: id,
-      limit: 100
-    }
     const { query } = req
     const { start, end } = query
     const startDate = start && getUnix(new Date(start))
@@ -36,17 +33,38 @@ export default async (req, res) => {
       customerTransactions.data.user = { firstName, lastName }
     }
 
-    if (startDate || endDate) {
-      opts.created = {}
-      if (startDate) {
-        opts.created.gte = startDate
+    let allCharges = []
+    let hasMore = true
+    let startingAfter = null
+
+    while (hasMore) {
+      const opts = {
+        customer: id,
+        limit: 100,
+        starting_after: startingAfter || undefined
       }
-      if (endDate) {
-        opts.created.lte = endDate
+
+      if (startDate || endDate) {
+        opts.created = {}
+        if (startDate) {
+          opts.created.gte = startDate
+        }
+        if (endDate) {
+          opts.created.lte = endDate
+        }
+      }
+
+      const chargesData = await stripe.charges.list(opts)
+
+      allCharges = allCharges.concat(chargesData.data)
+
+      hasMore = chargesData.has_more
+      if (hasMore) {
+        startingAfter = chargesData.data[chargesData.data.length - 1].id
       }
     }
-    const chargesData = await stripe.charges.list(opts)
-    customerTransactions.data.transactions = chargesData.data.map(charge => {
+
+    customerTransactions.data.transactions = allCharges.map((charge) => {
       const { amount, created } = charge
       return { amount, created }
     })
